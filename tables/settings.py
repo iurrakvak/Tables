6f9fd2396a75c4e0369285e0ca853490385fbe72
iurrakvak <iurrakvak@ukr.net>
2020-10-30 15:41:23 +0200

    Swap db to default, small fixes

--- tables/settings.py
+++ tables/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+SECRET_KEY = '7tasn*v36#hme@68e+6f5g15anzw^bjp5#4(7cfzou!s9)sb2s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
